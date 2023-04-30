f2025af5200527abec732ee6abc5a9703a3b8b9e
riraosan <fiatpanda750@icloud.com>
2023-04-30 15:08:21 +0900

    add cvbs sample

---
+++ examples/cvbs/basics_toCVBS.ino
@@ -0,0 +1,23 @@
+#include <M5GFX.h>
+#include <LGFX_8BIT_CVBS.h>
+#include <Avatar.h>
+
+using namespace m5avatar;
+
+static LGFX_8BIT_CVBS display;
+static M5Canvas sp_avatar(&display);
+
+Avatar avatar(&sp_avatar);
+
+void setup()
+{
+  M5.begin();
+  display.init();
+  avatar.init(1); // start drawing
+}
+
+void loop()
+{
+  // avatar's face updates in another thread
+  // so no need to loop-by-loop rendering
+}
